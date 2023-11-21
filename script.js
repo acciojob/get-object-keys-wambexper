349e1eb609e398a515205ed7cab5110ef82253ea
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-21 11:40:54 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Define the student object
+const student = {
+  name: 'John Doe'
+};
+
+// Add getKeys method to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Test the getKeys method on the student object
+console.log(student.getKeys()); // Output: ['name']
